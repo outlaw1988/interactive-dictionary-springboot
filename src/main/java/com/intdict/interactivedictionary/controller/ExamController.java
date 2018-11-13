6101c21946739cb8657026b5d00122cc7f815a06
Jakub Zamojski <kuba.zamojski@gmail.com>
2018-11-13 12:31:23 +0100

    Exam works

---
+++ src/main/java/com/intdict/interactivedictionary/controller/ExamController.java
@@ -0,0 +1,125 @@
+package com.intdict.interactivedictionary.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.intdict.interactivedictionary.model.Category;
+import com.intdict.interactivedictionary.model.Set;
+import com.intdict.interactivedictionary.model.Setup;
+import com.intdict.interactivedictionary.model.Word;
+import com.intdict.interactivedictionary.service.CategoryRepository;
+import com.intdict.interactivedictionary.service.SetRepository;
+import com.intdict.interactivedictionary.service.SetupRepository;
+import com.intdict.interactivedictionary.service.WordRepository;
+import com.intdict.interactivedictionary.utils.Utils;
+
+@Controller
+public class ExamController {
+
+	@Autowired
+	SetRepository setRepository;
+	
+	@Autowired
+	CategoryRepository categoryRepository;
+	
+	@Autowired
+	SetupRepository setupRepository;
+	
+	@Autowired
+	WordRepository wordRepository;
+	
+	@RequestMapping(value = "/exam-{setId}", method=RequestMethod.GET)
+	public String examInit(HttpServletRequest request, ModelMap model, @PathVariable(value="setId") int setId) {
+		
+		HttpSession session = request.getSession();
+		
+		Set set = setRepository.findById(setId).get();
+		//model.put("set", set);
+		session.setAttribute("set", set);
+		
+		Category category = categoryRepository.findById(set.getCategory().getId()).get();
+		//model.put("category", category);
+		session.setAttribute("category", category);
+		
+		List<Word> words = wordRepository.findBySet(set);
+		int size = words.size();
+		session.setAttribute("words", words);
+		session.setAttribute("size", size);
+		
+		ArrayList<Integer> shuffledIdxs = Utils.createShuffleList(size);
+		session.setAttribute("shuffledIdxs", shuffledIdxs);
+		
+		session.setAttribute("currWordIdx", 0);
+		session.setAttribute("currWordIdxToShow", 1);
+		session.setAttribute("corrAnsNum", 0);
+		
+		Word wordsToShow = words.get(shuffledIdxs.get(0));
+		session.setAttribute("currCorrAnswer", wordsToShow.getTargetWord());
+		
+		model.put("srcWord", wordsToShow.getSrcWord());
+		
+		ArrayList<Integer> answersList = Utils.initAnswersList(size);
+		session.setAttribute("answersList", answersList);
+		
+		session.setAttribute("setId", setId);
+		session.setAttribute("isCheckClicked", false);
+		
+		Setup setup = setupRepository.findBySet(set);
+		session.setAttribute("countdownDuration", setup.getCountdownDuration());
+		
+		return "exam";
+	}
+	
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value="exam-summary", method=RequestMethod.GET)
+	public String examSummary(HttpServletRequest request, ModelMap model) {
+		
+		HttpSession session = request.getSession();
+		Set set = (Set) session.getAttribute("set");
+		Setup setup = setupRepository.findBySet(set);
+		
+		model.put("lastResult", setup.getLastResult());
+		model.put("bestResult", setup.getBestResult());
+		model.put("srcLanguage", setup.getSrcLanguage());
+		model.put("targetLanguage", setup.getTargetLanguage());
+		
+		ArrayList<Integer> answersList = (ArrayList<Integer>) session.getAttribute("answersList");
+		
+		for (Integer item : answersList) {
+			System.out.println("answers list: " + item);
+		}
+		
+		List<Word> words = (List<Word>) session.getAttribute("words");
+		
+		ArrayList<String> srcWords = new ArrayList<>();
+		ArrayList<String> targetWords = new ArrayList<>();
+		
+		for (Word word : words) {
+			srcWords.add(word.getSrcWord());
+			targetWords.add(word.getTargetWord());
+		}
+		
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		Gson gson = gsonBuilder.create();
+		String srcWordsJson = gson.toJson(srcWords);
+		String targetWordsJson = gson.toJson(targetWords);	
+		
+		model.put("srcWords", srcWordsJson);
+		model.put("targetWords", targetWordsJson);
+		
+		return "exam-summary";
+	}
+	
+}
--- src/main/java/com/intdict/interactivedictionary/utils/Utils.java
+++ src/main/java/com/intdict/interactivedictionary/utils/Utils.java
@@ -1,5 +1,7 @@
 package com.intdict.interactivedictionary.utils;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Set;
 
 public class Utils {
@@ -19,4 +21,35 @@ public class Utils {
 		
 		return highestIdx;
 	}
+	
+	public static ArrayList<Integer> createShuffleList(int size) {
+		
+		ArrayList<Integer> outArr = new ArrayList<>();
+		
+		for (int i = 0; i < size; i++) {
+			outArr.add(i);
+		}
+		
+		Collections.shuffle(outArr);
+		return outArr;
+	}
+	
+	public static ArrayList<Integer> initAnswersList(int size) {
+		
+		ArrayList<Integer> outArr = new ArrayList<>();
+		
+		for (int i = 0; i < size; i++) {
+			outArr.add(-1);
+		}
+		
+		return outArr;
+	}
+	
+	public static ArrayList<Integer> assignValueToAnswersList(int idx, int value, 
+			ArrayList<Integer> answersList) {
+		
+		answersList.set(idx, value);
+		return answersList;
+	}
+
 }
